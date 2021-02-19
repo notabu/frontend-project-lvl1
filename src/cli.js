1b4e0b261d4ebe1270d2f9f776bb330bb37b9e5a
notabu <samyyklass@gmail.com>
2021-02-19 18:57:51 +0600

    fix cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
 import readlineSync from 'readline-sync';
 
+// eslint-disable-next-line import/prefer-default-export
 export const getUserName = () => {
   const userName = readlineSync.question('May I have your name? ');
-  console.log('Hi ' + userName + '!');
-}
+  console.log(`Hi ${userName}!`);
+};
