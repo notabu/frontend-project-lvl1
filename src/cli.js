543dc6599f2c9b20fc5f4479a7634ca2124f9510
notabu <samyyklass@gmail.com>
2021-02-20 21:36:22 +0600

    add var userName at cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,10 @@
 import readlineSync from 'readline-sync';
 
+// eslint-disable-next-line import/no-mutable-exports
+export let userName;
 // eslint-disable-next-line import/prefer-default-export
 export const getUserName = () => {
-  const userName = readlineSync.question('May I have your name? ');
+  const getName = readlineSync.question('May I have your name? ');
+  userName = getName;
   console.log(`Hi ${userName}!`);
 };
