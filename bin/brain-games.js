bf04b34a8d31c0f0e6258b76e069e3d72460f05d
notabu <samyyklass@gmail.com>
2021-02-20 21:53:47 +0600

    fix brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,13 +1,8 @@
 #!/usr/bin/env node
 import { getUserName } from '../src/cli.js';
-// eslint-disable-next-line import/named
-import { getUserAnswer } from './brain-even.js';
 
 // Приветствуем пользователя
 console.log('Welcome to the Brain Games!');
 
 // Запрашиваем имя пользователя и приветствуем его
 getUserName();
-
-// Запускаем игру
-getUserAnswer();
