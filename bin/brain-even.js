23502730b274ee229599cc44ac433a49a8cb6c12
notabu <samyyklass@gmail.com>
2021-02-22 13:41:26 +0600

    fix brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,36 +1,3 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
-import { getRandomNum } from '../src/rand.js';
-import { getUserName, userName } from '../src/cli.js';
-
-// function for checking a number for parity
-const isEven = (num) => (num % 2 === 0 ? 'yes' : 'no');
-
-const countSteps = 3;
-// eslint-disable-next-line import/prefer-default-export
-export const getUserAnswer = () => {
-  console.log('"yes" if the number is even, otherwise answer "no".');
-
-  for (let i = 0; i < countSteps; i += 1) {
-    // generate random number
-    const randomNum = getRandomNum();
-    console.log(`Question: ${randomNum}`);
-    const userAnswer = readlineSync.question('Your answer: ');
-
-    if (userAnswer === isEven(randomNum)) {
-      console.log('Correct!');
-    } else if (userAnswer !== isEven(randomNum)) {
-      return console.log(`'${userAnswer}' is wrong answer. Correct answer was '${isEven(randomNum)}'. Let's try again, ${userName}!`);
-    }
-  }
-  return console.log(`Congratulations, ${userName}`);
-};
-
-// Приветствуем пользователя
-console.log('Welcome to the Brain Games!');
-
-// Запрашиваем имя пользователя и приветствуем его
-getUserName();
-
-// Запускаем игру
-getUserAnswer();
+import playGame from '../src/games/even.js'
+playGame();
