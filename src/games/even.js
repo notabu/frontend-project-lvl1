3e29639707527a40ba6feff9ca06e33509e382ed
notabu <samyyklass@gmail.com>
2021-02-22 13:51:30 +0600

    fix even.js

--- src/games/even.js
+++ src/games/even.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-import getRandomNum from '../rand.js';
 import gameEngine from '../index.js';
+import getRandomNum from '../rand.js';
 
 // function for checking a number for parity
 const isEven = (num) => (num % 2 === 0);
