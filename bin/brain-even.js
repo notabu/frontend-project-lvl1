89f4792f1ff5df8ea9775189978f8548fa9b3d44
notabu <samyyklass@gmail.com>
2021-02-22 10:03:27 +0600

    fix brain-even

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -2,27 +2,32 @@
 import readlineSync from 'readline-sync';
 import { getUserName, userName } from '../src/cli.js';
 
-const numbers = [15, 6, 7];
+// function random number generator
+const getRandomNum = (min = 1, max = 10) => {
+  const rand = min + Math.random() * (max + 1 - min);
+  return Math.floor(rand);
+};
 
 // function for checking a number for parity
 const isEven = (num) => (num % 2 === 0 ? 'yes' : 'no');
 
+const countSteps = 3;
 // eslint-disable-next-line import/prefer-default-export
 export const getUserAnswer = () => {
   console.log('"yes" if the number is even, otherwise answer "no".');
 
-  // eslint-disable-next-line no-restricted-syntax
-  for (const number of numbers) {
-    console.log(`Question: ${number}`);
+  for (let i = 0; i < countSteps; i += 1) {
+    // generate random number
+    const randomNum = getRandomNum();
+    console.log(`Question: ${randomNum}`);
     const userAnswer = readlineSync.question('Your answer: ');
 
-    if (userAnswer === isEven(number)) {
+    if (userAnswer === isEven(randomNum)) {
       console.log('Correct!');
-    } else if (userAnswer !== isEven(number)) {
-      return console.log(`'${userAnswer}' is wrong answer. Correct answer was '${isEven(number)}'. Let's try again, ${userName}!`);
+    } else if (userAnswer !== isEven(randomNum)) {
+      return console.log(`'${userAnswer}' is wrong answer. Correct answer was '${isEven(randomNum)}'. Let's try again, ${userName}!`);
     }
   }
-
   return console.log(`Congratulations, ${userName}`);
 };
 
