0bc134900f94c7e24774b91069bfc8680a7055d3
notabu <samyyklass@gmail.com>
2021-02-23 14:07:02 +0600

    fix rand.js

--- src/rand.js
+++ src/rand.js
@@ -1,6 +1,6 @@
 // function random number generator
 // eslint-disable-next-line import/prefer-default-export
-const getRandomNum = (min = 1, max = 10) => {
+const getRandomNum = (min = 1, max = 9) => {
   const randNum = min + Math.random() * (max + 1 - min);
   return Math.floor(randNum);
 };
