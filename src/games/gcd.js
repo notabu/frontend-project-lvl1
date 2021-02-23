83ada88fd37b54235f3fccfee63e7230c9b37401
notabu <samyyklass@gmail.com>
2021-02-23 18:09:22 +0600

    added logic to gcd.js

---
+++ src/games/gcd.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+import gameEngine from '../index.js';
+import { getLargeRandomNum } from '../rand.js';
+
+const getRandomNum = () => {
+  const numbers = [];
+  for (let i = 0; i < 2; i += 1) {
+    numbers.push(getLargeRandomNum());
+  }
+  return numbers;
+};
+
+const getGCD = (firstNum, secondNum) => {
+  if (secondNum === 0) {
+    return firstNum;
+  }
+
+  return getGCD(secondNum, firstNum % secondNum);
+};
+
+const generateQuestion = () => {
+  const arrayNum = getRandomNum();
+  return `${arrayNum[0]} ${arrayNum[1]}`;
+};
+
+const generateRound = () => {
+  const questionText = generateQuestion();
+  const questionNum = questionText.split(' ');
+  // eslint-disable-next-line consistent-return
+  const correctAnswer = String(getGCD(Number(questionNum[0]), Number(questionNum[1])));
+  return { questionText, correctAnswer };
+};
+
+export default () => {
+  const conditionGame = 'What is the result of the expression?';
+  gameEngine(generateRound, conditionGame);
+};
