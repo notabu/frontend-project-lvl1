cd8e4f65bc29e7f605c0f81f16d4e8d7aea22976
notabu <samyyklass@gmail.com>
2021-02-25 22:33:52 +0600

    fix brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,4 @@
 #!/usr/bin/env node
 import { getUserName } from '../src/cli.js';
-console.log('/Hello, Tirion/');
+
+getUserName();
--- src/cli.js
+++ src/cli.js
@@ -4,6 +4,5 @@ import readlineSync from 'readline-sync';
 export let userName;
 export const getUserName = () => {
   const getName = readlineSync.question('May I have your name? ');
-  userName = getName;
-  console.log('Hello', userName);
+  console.log(`/Hello, ${getName}/`);
 };
