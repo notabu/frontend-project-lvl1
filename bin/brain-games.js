b1d4964319ecd05ef274cca8041274ae0c4db2cf
notabu <samyyklass@gmail.com>
2021-02-25 22:18:17 +0600

    restore bin/brain-games.js

---
+++ bin/brain-games.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+import { getUserName } from '../src/cli.js';
+
+// Приветствуем пользователя
+console.log('Welcome to the Brain Games!');
+
+// Запрашиваем имя пользователя и приветствуем его
+getUserName();
